b703f59d609edd4cf01013a478a2e2aa8af8eda1
Siddhanth J Ajri <y2jsiddajri@gmail.com>
2020-07-25 16:04:26 +0530

    class and func components

---
+++ app/src/components/Greet.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+// function Greet() {
+//    return <h1> Siddhanth is BACK! </h1>;
+// } 
+
+const Greet = () => <h1> SIDDHANTH IS BACK BAYBAY with func comp </h1>
+
+
+export default Greet;
