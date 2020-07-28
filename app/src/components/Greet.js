b6e91bec36e5dd69291450b0dfeaf9328a241033
Siddhanth J Ajri <y2jsiddajri@gmail.com>
2020-07-29 01:01:50 +0530

    destructuring (es6 compat)

--- app/src/components/Greet.js
+++ app/src/components/Greet.js
@@ -4,11 +4,25 @@ import React, { Component } from 'react';
 //    return <h1> Siddhanth is BACK! </h1>;
 // } 
 
-const Greet = (props) => {
+/* const Greet = (props) => {
     console.log(props);
     return (
         <div>
             <h1> Func comp:- Introducing {props.superstar} - Gimmick: {props.gimmick} </h1>
+            {props.children}
+        </div>
+    )
+    
+}
+ */
+
+
+const Greet = (props) => {
+    console.log(props);
+    const {superstar, gimmick} = props
+    return (
+        <div>
+            <h1> Func comp:- Introducing {superstar} - Gimmick: {gimmick} </h1>
             {props.children}
         </div>
     )
--- app/src/components/Welcome.js
+++ app/src/components/Welcome.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react';
 class Welcome extends Component {
     render() {
         console.log(this.props)
+        const {name, gimmick} = this.props
         return (
             <div>
-                <h1> CLASS COMPONENTS:- Introducing {this.props.superstar} with Gimmick: {this.props.gimmick} </h1>
+                <h1> CLASS COMPONENTS:- Introducing {name} with Gimmick: {gimmick} </h1>
                 {this.props.children}
             </div>
             
