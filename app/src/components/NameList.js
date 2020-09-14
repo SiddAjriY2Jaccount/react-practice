3b45d0bc4cf16a9b1fa2fffdec8584f78b3bd7df
Siddhanth J Ajri <y2jsiddajri@gmail.com>
2020-09-14 13:51:34 +0530

    key prop in lists

--- app/src/components/NameList.js
+++ app/src/components/NameList.js
@@ -4,20 +4,22 @@ import Person from './Person'
 function NameList() {
 
     const names = [
-        {
+        {   
+            id: 1,
             name : "Bruce Prichard",
             show : "Friday Night SmackDown",
             debut : "Paul Bosch' Houston Wrestling"
         },
 
         {
+            id: 2,
             name : "Paul Heyman",
             show : "Monday Night RAW",
             debut : "WCW NWA: The Dangerous Alliance"
         }
     ]
 
-    const nameList = names.map(name => <Person person={name}></Person>)
+    const nameList = names.map(name => <Person key={name.id} person={name}></Person>)
 
     return (
         <div>
